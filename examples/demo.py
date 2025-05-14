1525a4deeacf6a103a9198598523c87e60405d7d
AJ-cubes <jaina10@wis.edu.hk>
2025-05-14 13:07:27 +0800

    Update demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -1,1 +1,22 @@
+import customtkinter as ctk
+from CTkSeparator import CTkSeparator
 
+
+app = ctk.CTk()
+
+above = ctk.CTkLabel(master=app,
+                     text="Above the CTkSeparator")
+above.pack(pady=12, padx=10)
+test_separator = CTkSeparator(master=app,
+                              length=500,
+                              line_weight=4,
+                              dashes=10,
+                              fg_color="#FFFFFF",
+                              corner_radius=10,
+                              orientation='horizontal',
+                              gap=5)
+test_separator.pack(pady=12, padx=10)
+below = ctk.CTkLabel(master=app,
+                     text="Below the CTkSeparator")
+below.pack(pady=12, padx=10)
+app.mainloop()
