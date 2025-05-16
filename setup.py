dd1b18319263a8d4b474f18cb04a41dc78a24040
AJ-cubes <jaina10@wis.edu.hk>
2025-05-16 19:49:28 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CTkSeparator",
-    version="1.0",
+    version="1.1",
     author="AJ-cubes",
     description="A customizable separator widget for CustomTkinter",
     long_description=open("README.md").read(),
